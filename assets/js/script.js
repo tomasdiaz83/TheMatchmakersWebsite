24ed244baeb5a9c98ce0f484f38068ae606bc38a
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-09-12 15:50:36 -0500

    made carousel posters clickable and preparing to display movie information

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,41 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
 });
 
+function movieDataDisplay(movieInfo, y) {
+    console.log(movieInfo);
+    console.log(y);
+    console.log(movieInfo[y].Title);
+}
+
+function posterDisplay (data) {
+    console.log(data);
+    console.log(data.Search.length);
+    $("#movie-carousel").empty();
+
+    for(i=0; i < data.Search.length; i++) {
+        $("#movie-carousel")
+            .append('<a class="carousel-item" id = "item'+i+'" data-item ='+i+'><img src='+data.Search[i].Poster+'></a>')
+        $('#item'+i)
+            .click(function() {
+                movieDataDisplay(data.Search, this.dataset.item);
+            })
+    }
+
+    elementosCarousel = document.querySelectorAll('.carousel');
+    instances = M.Carousel.init(elementosCarousel, {
+            duration: 150,
+            shift: 5,
+            padding: 5,
+            numVisible: 5,
+
+    });
+
+    // if (data.totalRestults > 10) {
+    //     $('#container')
+    //         .append('<button id = next>Next</button>')
+    //         .append('<button id = next>Next</button>')
+    // }     
+}
 
 function movieSearch (title) {
     fetch ('http://www.omdbapi.com/?s='+title+'&type=movie&page=1&apikey=70c6cc67')
@@ -17,29 +52,7 @@ function movieSearch (title) {
             return response.json();
         })
         .then (function (data) {
-            console.log(data);
-            
-            $("#movie-carousel").empty();
-
-            for(i=0; i < 10; i++) {
-                $("#movie-carousel")
-                    .append('<a class="carousel-item"><img src='+data.Search[i].Poster+'></a>')                
-            }
-
-            elementosCarousel = document.querySelectorAll('.carousel');
-            instances = M.Carousel.init(elementosCarousel, {
-                    duration: 150,
-                    shift: 5,
-                    padding: 5,
-                    numVisible: 5,
-
-            });
-
-            // if (data.totalRestults > 10) {
-            //     $('#container')
-            //         .append('<button id = next>Next</button>')
-            //         .append('<button id = next>Next</button>')
-            // }
+            posterDisplay(data);
             
         })
 }
