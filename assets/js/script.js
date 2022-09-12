a0987614af77643b80bfcefd7e57b6ab44dd1ad5
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-09-12 14:05:18 -0500

    connected JS, receiving from form, fetching from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,42 @@ document.addEventListener('DOMContentLoaded', ()=>{
 });
 
 
+function movieSearch (title) {
+    fetch ('http://www.omdbapi.com/?s='+title+'&type=movie&page=1&apikey=70c6cc67')
+        .then (function (response) {
+            page = 1;
+            return response.json();
+        })
+        .then (function (data) {
+            console.log(data);
+            console.log(data.Search[0].Title);
+            // for(i=0; i < 10; i++) {
+            //     $('#container')
+            //         .prepend('<img src = '+ data.Search[i].Poster + '>')
+            //         .prepend('<p>'+data.Search[i].Title+'</p>');
 
+            // }
+            // if (data.totalRestults > 10) {
+            //     $('#container')
+            //         .append('<button id = next>Next</button>')
+            //         .append('<button id = next>Next</button>')
+            // }
+            
+        })
+}
 
+var form = document.querySelector('#movieSearch');
+var input = document.querySelector('#movieInput');
 
+form.addEventListener('submit', function(e){
+    e.preventDefault();
 
+    //getting city name
+    movie = input.value.trim();
 
+    //clear form
+    input.value = "";
+
+    //get weather
+    movieSearch(movie);
+})
