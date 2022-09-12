c98913fa3690a3fcd87ffe9a7fd04aa3cead430f
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-09-12 17:51:12 -0500

    Began building out a music search function utilizing the Spotify API through rapidAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
 });
 
-function movieDataDisplay(movieInfo, y) {
-    console.log(movieInfo);
-    console.log(y);
-    console.log(movieInfo[y].Title);
+//variables
+var formMovie = document.querySelector('#movieSearch');
+var inputMovie = document.querySelector('#movieInput');
+var formMusic = document.querySelector('#musicSearch');
+var inputMusic = document.querySelector('#musicInput');
+//search for movies
+function movieSearch (title) {
+    fetch ('http://www.omdbapi.com/?s='+title+'&type=movie&page=1&apikey=70c6cc67')
+        .then (function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            posterDisplay(data);
+            
+        })
 }
 
+//display posters in carousel
 function posterDisplay (data) {
     console.log(data);
     console.log(data.Search.length);
@@ -36,39 +48,79 @@ function posterDisplay (data) {
             padding: 5,
             numVisible: 5,
 
-    });
-
-    // if (data.totalRestults > 10) {
-    //     $('#container')
-    //         .append('<button id = next>Next</button>')
-    //         .append('<button id = next>Next</button>')
-    // }     
+    });   
 }
 
-function movieSearch (title) {
-    fetch ('http://www.omdbapi.com/?s='+title+'&type=movie&page=1&apikey=70c6cc67')
-        .then (function (response) {
-            page = 1;
-            return response.json();
-        })
-        .then (function (data) {
-            posterDisplay(data);
-            
-        })
+// ! display movie information -- Incomplete
+function movieDataDisplay(movieInfo, y) {
+    console.log(movieInfo);
+    console.log(y);
+    console.log(movieInfo[y].Title);
 }
 
-var form = document.querySelector('#movieSearch');
-var input = document.querySelector('#movieInput');
-
-form.addEventListener('submit', function(e){
+//accepting information from movie title search
+formMovie.addEventListener('submit', function(e){
     e.preventDefault();
 
     //getting city name
-    movie = input.value.trim();
+    movie = inputMovie.value.trim();
 
     //clear form
-    input.value = "";
+    inputMovie.value = "";
 
     //get movies
     movieSearch(movie);
+})
+
+function musicDisplay(data) {
+    console.log(data);
+    // ! This needs to be worked on
+    // for(i=0; i < data.Search.length; i++) {
+    //     $("#music-carousel")
+    //         .append('<a class="carousel-item" data-item ='+i+'><img src='+data.Search[i].Poster+'></a>')
+    //     $('#item'+i)
+    //         .click(function() {
+                
+    //         })
+    // }
+
+    elementosCarousel = document.querySelectorAll('.carousel');
+    instances = M.Carousel.init(elementosCarousel, {
+            duration: 150,
+            shift: 5,
+            padding: 5,
+            numVisible: 5,
+
+    });
+}
+
+//music API testing
+function musicSearch () {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8dd9082dcamshb3ce755dcfe2fcbp1d477cjsna72aacf57cc3',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify23.p.rapidapi.com/search/?q=love&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+        .then(response => response.json())
+        .then(function (data) {
+            musicDisplay(data);   
+        })
+        .catch(err => console.error(err));
+}
+
+formMusic.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //getting city name
+    music = inputMusic.value.trim();
+
+    //clear form
+    inputMusic.value = "";
+
+    //get movies
+    musicSearch(music);
 })
