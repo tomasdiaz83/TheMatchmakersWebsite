384ec1f40bc196e85c673505243365b73ded32ef
Lisbett Cortes <109909370+lcortes0801@users.noreply.github.com>
2022-09-12 08:37:38 -0500

    Add files via upload
    Initial project design

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
+
+    });
+});
+
+
+
+
+
+
+
