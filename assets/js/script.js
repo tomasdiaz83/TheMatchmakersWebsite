17350aebdf40030e6bbf68d76d9649c1a300ca47
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-09-15 21:39:07 -0500

    Broke the album display... Apparently doing the movie first breaks the album... Why? Maybe an overuse of data as a variable?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elementosCarousel, {
         duration: 150,
@@ -20,85 +20,87 @@ var albumSelector = document.querySelector('#album-selector');
 
 //array for paired selections
 var selectedPair = {
-    Movie : {
-        Title : '',
-        imdbLink : '',
-        Poster : ''
+    Movie: {
+        Title: '',
+        imdbLink: '',
+        Poster: ''
     },
-    Album : {
-        Title : '',
-        Link : '',
-        AlbumCover : ''
+    Album: {
+        Title: '',
+        Link: '',
+        AlbumCover: ''
     }
 };
 
-// ! display movie information in card -- Incomplete
+function displaySelections (selectedPair) {
+    console.log(selectedPair);
+    console.log("testing");
+    $('#selected-movie').src = selectedPair.Movie.Poster;
+    $('#selected-album').src = selectedPair.Album.Album;
+}
+
 function movieDataDisplay(movie) {
-    fetch ('http://www.omdbapi.com/?i='+movie+'&type=movie&apikey=70c6cc67&plot=short')
-        .then (function (response) {
+    fetch('http://www.omdbapi.com/?i=' + movie + '&type=movie&apikey=70c6cc67&plot=short')
+        .then(function (response) {
             return response.json();
         })
-        .then (function (data) {
-            $('#active-movie-poster').attr('src',data.Poster);
+        .then(function (data) {
+            $('#active-movie-poster').attr('src', data.Poster);
             $('#active-movie-title').html(data.Title);
             $('#active-movie-year').html(data.Year);
             $('#active-movie-plot').html(data.Plot);
-            $('#active-movie-imdb').html("<a href = 'https://www.imdb.com/title/"+data.imdbID+"' target = '_blank'>IMDB Data</a>");
+            $('#active-movie-imdb').html("<a href = 'https://www.imdb.com/title/" + data.imdbID + "' target = '_blank'>IMDB Data</a>");
         })
-
-    // console.log(movieInfo[y].Plot);
-    
-
 }
 
 //display posters in carousel
-function posterDisplay (data) {
+function posterDisplay(data) {
     console.log(data);
     console.log(data.Search.length);
     $("#movie-carousel").empty();
 
-    for(i=0; i < data.Search.length; i++) {
+    for (i = 0; i < data.Search.length; i++) {
         $("#movie-carousel")
             .append(`<a class= 'carousel-item' id = 'item${i}' data-item ='${i}'><img src='${data.Search[i].Poster}'></a>`)
-        $('#item'+i)
-            .click(function() {
+        $('#item' + i)
+            .click(function () {
                 movieDataDisplay(data.Search[this.dataset.item].imdbID);
             })
     }
 
     elementosCarousel = document.querySelectorAll('.carousel');
     instances = M.Carousel.init(elementosCarousel, {
-            duration: 150,
-            shift: 5,
-            padding: 5,
-            numVisible: 5,
+        duration: 150,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
 
-    });   
+    });
 }
 
 //movie api fetch
-function movieSearch (title) {
-    fetch ('http://www.omdbapi.com/?s='+title+'&type=movie&page=1&apikey=70c6cc67')
-        .then (function (response) {
+function movieSearch(title) {
+    fetch('http://www.omdbapi.com/?s=' + title + '&type=movie&page=1&apikey=70c6cc67')
+        .then(function (response) {
             return response.json();
         })
-        .then (function (data) {
+        .then(function (data) {
             posterDisplay(data);
         })
 }
 
 //add selected movie to pair
-movieSelector.addEventListener('click', function() {
+movieSelector.addEventListener('click', function () {
     selectedPair.Movie.Title = document.getElementById('active-movie-title').textContent;
     selectedPair.Movie.imdbLink = document.getElementById('active-movie-imdb').textContent;
     selectedPair.Movie.Poster = document.getElementById('active-movie-poster').src;
-    
-    console.log(selectedPair);
+
+    displaySelections(selectedPair);
 })
-    
+
 
 //accepting information from movie title search
-formMovie.addEventListener('submit', function(e){
+formMovie.addEventListener('submit', function (e) {
     e.preventDefault();
 
     //getting city name
@@ -115,8 +117,8 @@ formMovie.addEventListener('submit', function(e){
 function albumDataDisplay(albumInfo, y) {
     var spotifyLink = albumInfo[y].data.uri;
     spotifyLink = spotifyLink.split(":").pop();
-    
-    $('#active-music-poster').attr('src',albumInfo[y].data.coverArt.sources[0].url);
+
+    $('#active-music-poster').attr('src', albumInfo[y].data.coverArt.sources[0].url);
     $('#active-album-title').html(albumInfo[y].data.name);
     $('#active-artist').html(albumInfo[y].data.artists.items[0].profile.name);
     $('#active-spotify-link').html(`<a id = "selected-spotify-link" href = https://open.spotify.com/album/${spotifyLink} target = '_blank'>Listen on Spotify</a>`);
@@ -125,30 +127,30 @@ function albumDataDisplay(albumInfo, y) {
 //displaying albums in carousel
 function albumDisplay(data) {
     console.log(data);
-    
+
     $("#music-carousel").empty();
 
-    for(i=0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         $("#music-carousel")
-            .append('<a class="carousel-item" id = "item'+i+'" data-item ='+i+'><img src='+data.albums.items[i].data.coverArt.sources[0].url+'></a>')
-        $('#item'+i)
-            .click(function() {
+            .append('<a class="carousel-item" id = "item' + i + '" data-item =' + i + '><img src=' + data.albums.items[i].data.coverArt.sources[0].url + '></a>')
+        $('#item' + i)
+            .click(function () {
                 albumDataDisplay(data.albums.items, this.dataset.item);
             })
     }
 
     elementosCarousel = document.querySelectorAll('.carousel');
     instances = M.Carousel.init(elementosCarousel, {
-            duration: 150,
-            shift: 5,
-            padding: 5,
-            numVisible: 5,
+        duration: 150,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
 
     });
 }
 
 //music API fetch
-function musicSearch (artist) {
+function musicSearch(artist) {
     const options = {
         method: 'GET',
         headers: {
@@ -156,26 +158,26 @@ function musicSearch (artist) {
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
     };
-    
-    fetch('https://spotify23.p.rapidapi.com/search/?q='+artist+'&type=artist&offset=2&limit=10&numberOfTopResults=5', options)
+
+    fetch('https://spotify23.p.rapidapi.com/search/?q=' + artist + '&type=artist&offset=2&limit=10&numberOfTopResults=5', options)
         .then(response => response.json())
         .then(function (data) {
-            albumDisplay(data);   
+            albumDisplay(data);
         })
         .catch(err => console.error(err));
 }
 
 //add selected movie to pair
-albumSelector.addEventListener('click', function() {
+albumSelector.addEventListener('click', function () {
     selectedPair.Album.Title = document.getElementById('active-album-title').textContent;
     selectedPair.Album.Link = document.getElementById('selected-spotify-link').href;
     selectedPair.Album.AlbumCover = document.getElementById('active-music-poster').src;
-    
-    console.log(selectedPair);
+
+    displaySelections(selectedPair);
 })
 
 //getting music information from user
-formMusic.addEventListener('submit', function(e){
+formMusic.addEventListener('submit', function (e) {
     e.preventDefault();
 
     //getting city name
